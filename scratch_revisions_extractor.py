874c2c9436f47dd1822acc91bad4bfcf8a7c10d4
UncleSamTech <sanueloel3486@gmail.com>
2023-11-28 19:30:35 -0700

    wip

--- scratch_revisions_extractor.py
+++ scratch_revisions_extractor.py
@@ -163,7 +163,7 @@ def get_revisions_and_run_parser(cwd, project_name, main_branch, debug=False):
                 root_name = Path(new_original_file_name).stem
                 # suggestion: save the original file name extension here to avoid manual fixes later :(
             
-                com = f'/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_contents'
+                com = f'/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/projects'
                 com = com + "/" +  project_name + "/" + new_original_file_name + "_CMMT_" + c + ".json"
                 print(com)
             
@@ -234,6 +234,9 @@ def main2(project_path: str):
             main_branch = main_branch.stdout.decode("utf-8").strip('/n')[0:]
             if len(main_branch) > 1 or main_branch != '' or main_branch != None and repo != '' or repo != None and len(repo) > 0 and len(main_branch) > 0:
                 try:
+                    print(repo)
+                    print(proj_name)
+                    print(main_branch)
                     v = get_revisions_and_run_parser(repo, proj_name, main_branch)
                     if v == -1:
                         #logging.error(f'no sb3 file found in {project_name} due to {logging.ERROR}')
