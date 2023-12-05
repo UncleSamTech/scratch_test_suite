ed7282ad6926f82a942ebbe628b234eb94f9958a
UncleSamTech <sanueloel3486@gmail.com>
2023-12-05 01:39:34 -0700

    wip

--- scratch_revision.sh
+++ scratch_revision.sh
@@ -1,6 +1,6 @@
-INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/revisions_projectnames.txt
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/revisions_projectnames2.txt
 while read p; do
     folder= echo $p 
-    mkdir /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/projects/$p
+    mkdir /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/projects2/$p
     #python3 /media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/scratch_revisions_extractor.py
 done < $INPUT
