127de7cf2332e4ed22e947fd2f28c6e1c339ee1e
UncleSamTech <sanueloel3486@gmail.com>
2023-12-04 18:33:22 -0700

    wip

--- author_commit.py
+++ author_commit.py
@@ -11,11 +11,9 @@ def author_commit(path):
                 if not os.listdir(f'{path}/{i}'):
                     continue
                 else:
-                    all_projects.append(i)
-                    #subprocess.call(['sh', '/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/author_commit_message.sh'])
+                    with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/projnames.txt","w") as wf:
+                        wf.write("{}\n".format(i))
+                    subprocess.call(['sh', '/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/author_commit_message.sh'])
         
-        with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/projnames.txt","w") as wf:
-            if len(all_projects) > 0:
-                for i in all_projects:
-                    wf.write("{}\n".format(i))
-author_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects")
+                    
+author_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/projects")
