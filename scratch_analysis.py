1e8c7e8a1c1298cef35b2ac62498532171bc34cf
UncleSamTech <sanueloel3486@gmail.com>
2023-12-06 18:49:16 -0700

    wip

--- scratch_analysis.py
+++ scratch_analysis.py
@@ -4,12 +4,19 @@ import os
 
 
 
-def plot_histogram_per_distribution(file_path:str,plot_result_path,xlabel,ylabel,title,fig_title,column_title):
+def plot_histogram_per_distribution(file_path:str,plot_result_path,xlabel,ylabel,title,fig_title,column_title,bins_count):
     df = pd.read_csv(file_path)
-    val = df[column_title].values
-    #edges = df['Edges'].values
+    grouped = df.groupby(column_title)
 
-    plt.hist(val,color='lightblue', ec='black',bins=20)
+    #val = df[column_title].values
+    
+    #Find the row count in each group
+    row_count = grouped.size()
+
+    #Display the result
+    data = row_count.values
+
+    plt.hist(data,color='lightblue', ec='black',bins=bins_count)
     plt.yscale('log')
     plt.ticklabel_format(axis='x',style='plain')
     plt.xlabel(xlabel)
@@ -17,6 +24,8 @@ def plot_histogram_per_distribution(file_path:str,plot_result_path,xlabel,ylabel
     plt.title(title)
     plt.show()
     return plt.savefig(f'{plot_result_path}/{fig_title}.pdf')
+
+
 
 def merge_csv_files(csv_file1,csv_file2,new_file_name):
     csv1 = pd.read_csv(csv_file1)
@@ -26,6 +35,6 @@ def merge_csv_files(csv_file1,csv_file2,new_file_name):
 
 
 
-merge_csv_files("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/authors_hashed2_for_merge.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_messages_uniq_proje_for_merge.csv","authors_project_unique_filtered_hashed2")
-#plot_histogram_per_distribution("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/nodes_edges/nodes_edges_folder/nodes_edges_per_file2.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/all_plots_results","Number of Edges Per File","Number of Files (Log Scale)","Histogram of Number of Edges Per Scratch(sb3) Files","edges_distribution_per_file","Edges")
+#merge_csv_files("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/authors_hashed2_for_merge.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_messages_uniq_proje_for_merge.csv","authors_project_unique_filtered_hashed2")
+plot_histogram_per_distribution("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_author_name_email_commit_name_commit_email_proj_name_commit_message.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/all_plots_results","Number of Authors-Committers Per Scratch(sb3) Project","Number of Scratch Projects (Log Scale)","Histogram of Number of Authors-Committers Per Scratch Project","authors_committers_distribution_per_projects","Project_Name",15)
 #/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary
