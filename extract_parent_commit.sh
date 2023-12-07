77c7282ddcc34dd88548980fa00978d212905ef7
UncleSamTech <sanueloel3486@gmail.com>
2023-12-06 19:48:32 -0700

    wip

--- extract_parent_commit.sh
+++ extract_parent_commit.sh
@@ -1,4 +1,4 @@
-INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/projnames2.txt
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/proj_branch/projectnames_branch_names2.csv
 while read p; do
     folder=`echo $p | cut -d\/ -f2`
     cd /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted/$folder
