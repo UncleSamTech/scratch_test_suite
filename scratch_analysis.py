940f4a0e77adb239712a9cbf47fd855cdb02980b
UncleSamTech <sanueloel3486@gmail.com>
2023-12-06 18:54:35 -0700

    wip

--- scratch_analysis.py
+++ scratch_analysis.py
@@ -6,8 +6,11 @@ import os
 
 def plot_histogram_per_distribution(file_path:str,plot_result_path,xlabel,ylabel,title,fig_title,column_title,bins_count):
     df = pd.read_csv(file_path)
-    grouped = df.groupby(column_title)
 
+    
+    grouped = df.groupby(column_title)
+    author_count = grouped["Author_Name"].nunique()
+    data2 = author_count.values
     #val = df[column_title].values
     
     #Find the row count in each group
@@ -16,7 +19,7 @@ def plot_histogram_per_distribution(file_path:str,plot_result_path,xlabel,ylabel
     #Display the result
     data = row_count.values
 
-    plt.hist(data,color='lightblue', ec='black',bins=bins_count)
+    plt.hist(data2,color='lightblue', ec='black',bins=bins_count)
     plt.yscale('log')
     plt.ticklabel_format(axis='x',style='plain')
     plt.xlabel(xlabel)
@@ -36,5 +39,5 @@ def merge_csv_files(csv_file1,csv_file2,new_file_name):
 
 
 #merge_csv_files("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/authors_hashed2_for_merge.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_messages_uniq_proje_for_merge.csv","authors_project_unique_filtered_hashed2")
-plot_histogram_per_distribution("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_author_name_email_commit_name_commit_email_proj_name_commit_message.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/all_plots_results","Number of Authors-Committers Per Scratch(sb3) Project","Number of Scratch Projects (Log Scale)","Histogram of Number of Authors-Committers Per Scratch Project","authors_committers_distribution_per_projects","Project_Name",15)
+plot_histogram_per_distribution("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_author_name_email_commit_name_commit_email_proj_name_commit_message.csv","/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/all_plots_results","Number of Authors Per Scratch(sb3) Project","Number of Scratch Projects (Log Scale)","Histogram of Number of Authors Per Scratch Project","authors_distribution_per_projects","Project_Name",15)
 #/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary
