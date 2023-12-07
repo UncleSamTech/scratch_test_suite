d1cf300131f012a19b2251b9868558c705cf4e81
UncleSamTech <sanueloel3486@gmail.com>
2023-12-06 19:37:02 -0700

    wip

--- extract_parent_commit.sh
+++ extract_parent_commit.sh
@@ -2,5 +2,5 @@ INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/
 while read p; do
     folder=`echo $p | cut -d\/ -f2`
     cd /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted/$folder
-    git --no-pager log --all --pretty=tformat: "%H %P" > /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commit/$p.txt
+    git --no-pager log --all --pretty=tformat:"%H %P" > /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commit/$p.txt
 done < $INPUT
