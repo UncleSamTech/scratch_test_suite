a76c8879a888893518ee76db81a1514e7c4a4f16
UncleSamTech <sanueloel3486@gmail.com>
2023-12-06 20:29:48 -0700

    wip

--- author_commit.py
+++ author_commit.py
@@ -20,5 +20,25 @@ def author_commit(path):
                         f.write("{}\n".format(e))
                         f.close()
         
+def parent_commit(path):
+    if os.path.isdir(path):
+        for i in os.listdir(path):
+            with open(os.path.join(path,i),'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    data = line.split(" ")
+                    commit_sha = data[0]
+                    parent_sha = data[1:]
+
+                    if len(parent_sha) == 0:
+                        with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commits.csv","a") as pc:
+                            pc.write(commit_sha + "," + "None\n")
+                    else:
+                        for p in parent_sha:
+                            with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commits.csv","a") as pc:
+                                pc.write(commit_sha + "," + p)
+
                     
-author_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted")
+#author_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted")
+parent_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commit")
