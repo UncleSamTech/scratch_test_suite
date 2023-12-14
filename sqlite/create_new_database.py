fa8dcfa62da43bb557f90ce0e6e3548dfc239509
UncleSamTech <sanueloel3486@gmail.com>
2023-12-14 07:12:52 -0700

    wip

--- sqlite/create_new_database.py
+++ sqlite/create_new_database.py
@@ -2,28 +2,28 @@ import sqlite3
 import pandas as pd
 
 # step 1: load data file
-#df = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/revisions_projects/proj_branch/projectnames_branch_names2.csv')
+df = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/projects.csv')
 #df3 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/authors_hashed2.csv')
 #df4 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_messages_unique2.csv')
-df5 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/content_parents_unique1.csv')
+#df5 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/content_parents_unique1.csv')
 
 
 # step 2: clean data
-#df.columns = df.columns.str.strip()
+df.columns = df.columns.str.strip()
 #df3.columns = df3.columns.str.strip()
 #df4.columns = df4.columns.str.strip()
-df5.columns = df5.columns.str.strip()
+#df5.columns = df5.columns.str.strip()
 
 
 # step 3: create/connect to database
 connection = sqlite3.connect("scratch_revisions_database.db")
 
 # step 4: load data file to sqlite
-#df.to_sql("Projects", connection, if_exists='replace', index=False)
+df.to_sql("Projects", connection, if_exists='replace', index=False)
 #df3.to_sql("Authors", connection, if_exists='replace', index=False)
 #df4.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
 #df4.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
-df5.to_sql("Content_Parents",connection,if_exists='replace',index=False)
+#df5.to_sql("Content_Parents",connection,if_exists='replace',index=False)
 
 #create the revision table to be used later
 #revision_obj = connection.cursor()
