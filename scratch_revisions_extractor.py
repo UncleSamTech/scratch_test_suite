009a3b140c519e3a5af6ed1480b532389e3e5f3d
samueliwuchukwu <samueloel3486@gmail.com>
2024-01-22 11:28:08 -0700

    wip

--- scratch_revisions_extractor.py
+++ scratch_revisions_extractor.py
@@ -186,6 +186,7 @@ def get_revisions_and_run_parser(cwd,main_branch,project_name, debug=False):
                 
                 
                 try:
+                    print('entered decoding')
                     val = sp.decode_scratch_bytes(contents1.stdout)
             
                     file_contents = val
