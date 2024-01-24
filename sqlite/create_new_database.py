713a5725d2aa0b9a1d82b4452c06c6d4b0de4d30
samueliwuchukwu <samueloel3486@gmail.com>
2024-01-24 00:17:09 -0700

    wip

--- sqlite/create_new_database.py
+++ sqlite/create_new_database.py
@@ -1,15 +1,15 @@
 import sqlite3
-import pandas as pd
+#import pandas as pd
 
 # step 1: load data file
-df = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/projects.csv')
+#df = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/projects.csv')
 #df3 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/authors_hashed2.csv')
 #df4 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auth_commit_summary/summary/commit_messages_unique2.csv')
 #df5 = pd.read_csv('/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/content_parents_unique1.csv')
 
 
 # step 2: clean data
-df.columns = df.columns.str.strip()
+#df.columns = df.columns.str.strip()
 #df3.columns = df3.columns.str.strip()
 #df4.columns = df4.columns.str.strip()
 #df5.columns = df5.columns.str.strip()
@@ -17,23 +17,23 @@ df.columns = df.columns.str.strip()
 
 # step 3: create/connect to database
 connection = sqlite3.connect("scratch_revisions_database.db")
-
+#connection2 = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_database.db")
 # step 4: load data file to sqlite
-df.to_sql("Projects", connection, if_exists='replace', index=False)
+#df.to_sql("Projects", connection, if_exists='replace', index=False)
 #df3.to_sql("Authors", connection, if_exists='replace', index=False)
 #df4.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
 #df4.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
 #df5.to_sql("Content_Parents",connection,if_exists='replace',index=False)
 
 #create the revision table to be used later
-#revision_obj = connection.cursor()
+#revision_obj = connection2.cursor()
 #revision_table = """CREATE TABLE Revisions (Project_Name,File, Revision, Commit_SHA, Commit_Date, Hash, Nodes, Edges); """
 #revision_obj.execute(revision_table)
 
 #create the hash table to be used later
-#hash_obj = connection.cursor()
-#hash_table = """CREATE TABLE Hashes (Hash,Content); """
-#hash_obj.execute(hash_table)
+hash_obj = connection.cursor()
+hash_table = """CREATE TABLE Contents (Hash,Content); """
+hash_obj.execute(hash_table)
 
 
 # step 5: close 
