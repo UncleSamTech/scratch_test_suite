c8427baa83fe61b095cf0c3d716582eaa8d5a579
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-03 21:20:41 -0700

    wip

--- extract_scratch_revision.py
+++ extract_scratch_revision.py
@@ -108,10 +108,12 @@ def get_connection2():
     cursor =  conn.cursor()
     return conn,cursor
 
+'''
 def escape_special_chars(input_string):
     escaped_string = subprocess.check_output(['echo', input_string], universal_newlines=True)
     escaped_string = subprocess.check_output(['sed', 's/[\\\/^$.*+?()[\]{}|[:space:]]/\\\\&/g'], input=escaped_string, universal_newlines=True)
     return escaped_string.strip()
+'''
 
 def get_all_projects_in_db():
     select_projects = """SELECT Project_Name from revisions;"""
@@ -170,7 +172,7 @@ def get_revisions_and_run_parser(cwd, main_branch,project_name,  debug=False):
     # for all sb3 files in ths project
         for f in filenames:
             #f = escape_special_chars(f)
-            proc1 = subprocess.run(['git --no-pager log -z --numstat --follow --pretty=tformat:"{}¬%H" -- "{}"'.format(escape_special_chars(f),escape_special_chars(f))], stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            proc1 = subprocess.run(['git --no-pager log -z --numstat --follow --pretty=tformat:"{}¬%H" -- "{}"'.format(f,f)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
             
             proc2 = subprocess.run(["cut -f3"], input=proc1.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
             
