a058e1bd040aff374cb937f8f376917e9ad1615c
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-05 14:53:58 -0700

    wip

---
+++ models_gram/nltk/scratch_test_train_generate.py
@@ -0,0 +1,23 @@
+import os
+import pickle
+from sklearn.model_selection import train_test_split
+
+def test_train_test_split(pickle_data,test_data_name,train_data_name):
+    data_load = None
+    with open(pickle_data,'rb') as file:
+        data_load = pickle.load(file)
+
+    train_data,test_data = train_test_split(data_load,test_size=0.1,random_state=42)
+
+    with open(train_data_name,"w") as file_train:
+        for token in train_data:
+            file_train.write(token + '\n')
+    
+    with open(test_data_name,'w') as file_test:
+        for token in test_data:
+            file_test.write(token + '\n')
+
+
+test_train_test_split("scratch_data.pkl","scratch_test_data_10.txt","scratch_train_data_90.txt")
+
+    
