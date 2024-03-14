d87540416d9df5495b5732a9db63307a4333d338
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-13 23:07:47 -0600

    wip

--- models_gram/nltk/scratch_train_mle.py
+++ models_gram/nltk/scratch_train_mle.py
@@ -36,10 +36,10 @@ class scratch_train_mle:
         with open(model_name,"rb") as f:
             self.loaded_scratch_model = pickle.load(f)
             #print(type(self.loaded_scratch_model))
-            #print(self.loaded_scratch_model.vocab)
+            print(self.loaded_scratch_model.vocab)
             #print(self.loaded_scratch_model.counts("event_whenflagclicked"))
-            #print(self.loaded_scratch_model.score("event_whenflagclicked"))
-            #print(self.loaded_scratch_model.vocab.lookup("switch"))
+            print(self.loaded_scratch_model.score("event_whenflagclicked"))
+            print(self.loaded_scratch_model.vocab.lookup("procedures"))
         return self.loaded_scratch_model
     
    
@@ -97,6 +97,6 @@ class scratch_train_mle:
         return accuracy,precision,recall
     
 tr_scr = scratch_train_mle()
-tr_scr.train_mle("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_train_data_90.txt",2,"/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_trained_model_version2")
-#tr_scr.load_trained_model("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_trained_model_version2_5.pkl")
+#tr_scr.train_mle("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_train_data_90.txt",2,"/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_trained_model_version2")
+tr_scr.load_trained_model("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_trained_model_version2_2.pkl")
 #tr_scr.scratch_evaluate_model_nltk("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_test_data_10.txt","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_models_ngram/scratch_trained_model_version2_5.pkl") 
