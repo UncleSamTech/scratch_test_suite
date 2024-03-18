ddcf4772244f87f17685f57802d08fdbb2650a79
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-18 14:10:29 -0600

    wip

---
+++ models_gram/nltk/kenlm_train.py
@@ -0,0 +1,36 @@
+import os
+#import kenlm
+import sys
+import nltk
+import subprocess
+
+class kenlm_train:
+
+    def __init__(self):
+        self.result = []
+        self.tokenized_data = ""
+
+    def tokenize_kenlm(self,train_data):
+        for line in train_data:
+            for sentence in nltk.sent_tokenize(line):
+                token_sentlist = nltk.word_tokenize(sentence)
+                self.tokenized_data += ''.join(token_sentlist).lower()
+        return self.tokenized_data
+
+    
+    def access_train_data_kenlm(self,file_path,cwd):
+        if os.path.isfile(file_path):
+            with open(file_path,"r") as each_sentence:
+                each_line = each_sentence.readlines()
+                val = self.tokenize_kenlm(each_line)
+                module_train = subprocess.run('',stdout=subprocess.PIPE, cwd=cwd, shell=True)
+
+
+kn = kenlm_train()
+
+
+
+
+print(kn.main("Whereupon neither the first testament was dedicated without blood.")) 
+
+        
