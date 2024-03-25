fdb4c8b753ec16206004e15c5c7e8dcfd9f01273
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-25 13:25:03 -0600

    wip

--- extract_scratch_paths.py
+++ extract_scratch_paths.py
@@ -1,7 +1,7 @@
-#import networkx as nx
+import networkx as nx
 import sqlite3
 import json
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 class Scratch_Path:
@@ -159,12 +159,14 @@ class Scratch_Path:
                                         ]
 
     def replace_non_vs_string_with_tokens(self,string_val):
-        if isinstance(string_val,str):
+        if isinstance(string_val,str) and len(string_val) > 0:
             val2 = string_val.split()
             print("see tokens" , val2)
-            new_list = ['<S>' if word not in self.valid_opcodes and word not in self.valid_other_field_codes else word for word in val2  ]
+            new_list = ['<S>' if word not in self.valid_opcodes and word not in self.valid_other_field_codes  else word for word in val2  ]
             print("replaced tokens" , new_list)
             return " ".join(new_list)
+        else:
+            return ""
 
 
 
@@ -181,7 +183,7 @@ class Scratch_Path:
     
     def get_all_contents(self,hash):
         int_val = None
-        conn,curr = self.get_connection2()
+        conn,curr = self.get_connection()
         if conn != None:
          curr.execute("select distinct(content) from contents where hash = ? ", (hash,))  
          try:
@@ -257,10 +259,10 @@ class Scratch_Path:
                                         sec_val  = self.replace_non_vs_string_with_tokens(val)
                                         
                                     except:
-                                        val = ''
-                                    if val is None or len(val) < 1:
+                                        sec_val = ''
+                                    if sec_val is None or len(sec_val) < 1:
                                         continue
-                                    fp.write(val + " ")
+                                    fp.write(sec_val + " ")
                                     fp.write("\n")
                                 else:
                                     continue
@@ -280,8 +282,8 @@ sc_path = Scratch_Path()
 #print(sc_path.get_all_hashes("/Users/samueliwuchukwu/documents/scratch_database/sc_hash_local.txt"))
 #print(sc_path.generate_simple_graph("/Users/samueliwuchukwu/documents/scratch_database/sc_hash_local.txt"))
 
-#sc_path.generate_simple_graph("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/list_of_hashes/sc_hash_local2.txt","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/list_of_hashes/extracted_paths3/")
-sc_path.generate_simple_graph("/Users/samueliwuchukwu/documents/scratch_database/scratch_local_hash2.txt","/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/sb3_parsed/extracted_paths/")
+sc_path.generate_simple_graph("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/list_of_hashes/sc_hash_local2.txt","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/list_of_hashes/extracted_paths4/")
+#sc_path.generate_simple_graph("/Users/samueliwuchukwu/documents/scratch_database/scratch_local_hash2.txt","/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/sb3_parsed/extracted_paths/")
 
 #v = sc_path.get_all_contents("cfbab365b6dd7f4138823df8ff2e89a108f43dbf8c9950ab27ac8cc981b9adac")
 #vis = sc_path.visualize_graph(gr)
