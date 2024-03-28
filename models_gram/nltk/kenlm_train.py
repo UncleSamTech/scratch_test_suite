b0f3244f1b3df6c1f86b148a496399025eb5c289
samueliwuchukwu <samueloel3486@gmail.com>
2024-03-28 12:49:06 -0600

    wip

--- models_gram/nltk/kenlm_train.py
+++ models_gram/nltk/kenlm_train.py
@@ -95,12 +95,14 @@ class kenlm_train:
                 #line=line.strip()
                 
                 if line == "\\1-grams": 
-                    token=line.split()[1]
-                    print(token)
-                    with open(vocab_file,"a") as vf:
-                        vf.write(token+"\n") 
-                elif line == "\\2-grams:":
-                    break
+                    one_gram_seen = True
+                    if one_grams_seen:
+                        token=line.split()[1]
+                        print(token)
+                    #with open(vocab_file,"a") as vf:
+                        #vf.write(token+"\n") 
+                #elif line == "\\2-grams:":
+                    #break
                 
 
 
