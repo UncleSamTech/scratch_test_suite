ead35921d0f3d5f84a8787bb5d9e6d337e8b54ad
samueliwuchukwu <samueloel3486@gmail.com>
2024-05-13 20:25:24 -0600

    wip

--- extract_scratch_revision.py
+++ extract_scratch_revision.py
@@ -135,7 +135,12 @@ def correct_code_replace2(byte_val):
     return inp
 
 def get_connection2():
-    conn = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_database.db",isolation_level=None)
+    conn = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_main_test.db",isolation_level=None)
+    cursor =  conn.cursor()
+    return conn,cursor
+
+def get_connection2_train():
+    conn = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_main_train.db",isolation_level=None)
     cursor =  conn.cursor()
     return conn,cursor
 
@@ -145,7 +150,22 @@ def get_all_projects_in_db():
     select_projects = """SELECT Project_Name from revisions;"""
     val = []
     fin_resp = []
-    conn,curr = get_connection()
+    conn,curr = get_connection2()
+    if conn != None:
+         curr.execute(select_projects)  
+         val = curr.fetchall()
+         fin_resp = [eac_val for each_cont in val if isinstance(val,list) and len(val) > 0 for eac_val in each_cont if isinstance(each_cont,tuple)]
+                     
+    else:
+        print("connection failed")
+    conn.commit()
+    return fin_resp
+
+def get_all_projects_in_db_train():
+    select_projects = """SELECT Project_Name from revisions;"""
+    val = []
+    fin_resp = []
+    conn,curr = get_connection2_train()
     if conn != None:
          curr.execute(select_projects)  
          val = curr.fetchall()
@@ -178,7 +198,7 @@ def split_file(file_byte):
     if isinstance(file_byte,bytes):
         return file_byte.decode().strip().split('\n')
 
-def get_revisions_and_run_parser(cwd, main_branch,project_name,  debug=False):
+def get_revisions_and_run_parser(cwd, main_branch,project_name , debug=False):
     sp = scratch_parser()
     
     proc1 = subprocess.run(['git --no-pager log --pretty=tformat:"%H" {} --no-merges'.format(main_branch)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
@@ -362,7 +382,7 @@ def get_revisions_and_run_parser(cwd, main_branch,project_name,  debug=False):
                 insert_revision_statement = """INSERT INTO Revisions (Project_Name, File, Revision, Commit_SHA, Commit_Date, Hash, Nodes, Edges) VALUES(?,?,?,?,?,?,?,?);"""
                 insert_hash_statement = """INSERT INTO Contents (Hash,Content) VALUES(?,?);"""
                 tree_value = str(json_output)
-                conn,cur = get_connection()
+                conn,cur = get_connection2_train()
                 val = None
                 if conn != None:
                     cur.execute(insert_revision_statement,(project_name,new_original_file_name,new_name,c,parsed_date_str,hash_value,nodes_count,edges_count))
@@ -376,6 +396,202 @@ def get_revisions_and_run_parser(cwd, main_branch,project_name,  debug=False):
             
         
 
+def get_revisions_and_run_parser_test(cwd, main_branch,project_name, debug=False):
+    sp = scratch_parser()
+    
+    proc1 = subprocess.run(['git --no-pager log --pretty=tformat:"%H" {} --no-merges'.format(main_branch)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
+    proc2 = subprocess.run(['xargs -I{} git ls-tree -r --name-only {}'], input=proc1.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+    
+    proc3 = subprocess.run(['grep -i "\\.sb3$"'], input=proc2.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+    
+    proc4 = subprocess.run(['sort -u'], input=proc3.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+    
+    filenames = proc4.stdout.decode().strip().split('\n')
+    
+
+    if len(filenames) > 1:
+    # for all sb3 files in ths project
+        for f in filenames:
+            #f = escape_special_chars(f)
+            proc1 = subprocess.run(['git --no-pager log -z --numstat --follow --pretty=tformat:"{}¬%H" -- "{}"'.format(f,f)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            
+            proc2 = subprocess.run(["cut -f3"], input=proc1.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            
+            
+            proc3_ = correct_code_replace(proc2.stdout)
+            
+            
+            proc4 = subprocess.run(['xargs -0 echo'], input=proc3_, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            
+            #decoded_proc4 = proc4.stdout.decode()
+            
+            filename_shas = proc4.stdout.decode().strip().split('\n')
+            
+            filename_shas = [x for x in filename_shas if x != '']
+            
+            #if 2 ¬ then it is the original filename that we are trying to trace back (includes original filename, commit)
+            #if 3 ¬ then it is not renamed (includes renamed filename, original filename, commit)
+            #if starts with ¬ then it is renamed (includes pre-filename, renamed filename, original filename, commit)
+            #if 1 ¬ then it is a beginning file with no diff; skip
+        
+            proc1 = subprocess.run(['git --no-pager log --all --pretty=tformat:"%H" -- "{}"'.format(f)], stdout=subprocess.PIPE, cwd=cwd, shell=True) # Does not produce renames
+            all_shas = proc1.stdout.decode().strip().split('\n') 
+            all_shas = [x for x in all_shas if x != '']
+            all_sha_names = {}
+        
+            for x in all_shas:
+                all_sha_names[x] = None
+
+            # get filenames for each commit
+            
+            for fn in filename_shas: # start reversed, oldest to newest
+                
+                #print("filename",fn)
+                separator_count = count_seperator(fn)
+                #separator_count = fn.strip().count("#")
+                
+                
+                
+                
+            
+                split_line = fn.strip('¬').split('¬')
+                #split_line = fn.strip('#').split('#')
+                
+                file_contents = ''
+            
+                if separator_count == 2:
+                    c = split_line[-1]
+
+                    if not is_sha1(c):
+                        # Edge case where line doesn't have a sha
+                        #print(split_line)
+                        continue
+
+                    all_sha_names[c] = split_line[0]
+                    
+                    #print("Separator count 2: assigning {} to {}".format(c, split_line[0]))
+        
+                elif fn[0] == '¬':
+                #elif fn[0] == '#':
+                    new_name = split_line[0]
+                    
+                    c = split_line[-1]
+
+                    if not is_sha1(c):
+                        
+                        continue
+
+                    all_sha_names[c] = new_name
+                    
+                
+                elif separator_count == 3:
+                    # print(split_line[-1])
+                    new_name = split_line[0]
+                    
+                    c = split_line[-1]
+
+                    if not is_sha1(c):
+                        # Edge case where line doesn't have a sha
+                        #print(split_line)
+                        continue
+
+                    all_sha_names[c] = new_name
+                    #print("Separator count 3: assigning {} to {}".format(c, split_line[-3]))
+                
+                elif separator_count == 1:
+                    continue
+        
+                else:
+                    raise ValueError('Unknown case for file')
+ 
+
+
+            all_sha_dates = {}
+            for c in all_sha_names.keys():
+                commit_date = subprocess.run(['git log -1 --format=%ci {}'.format(c)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, cwd=cwd, shell=True).stdout.decode()
+                parsed_date = datetime.strptime(commit_date.strip(), '%Y-%m-%d %H:%M:%S %z')
+            
+
+                all_sha_dates[c] = parsed_date
+
+            # fill in the gaps
+            prev_fn = f
+            for c in all_sha_names.keys():
+                if all_sha_names[c] is None:
+                    all_sha_names[c] = prev_fn
+                prev_fn = all_sha_names[c]
+        
+            stats = {}
+
+        
+            
+            for c in sorted(all_sha_dates, key=all_sha_dates.get): # start oldest to newest
+           
+                new_name = all_sha_names[c]
+            
+                
+                commit_date = subprocess.run(['git log -1 --format=%ci {}'.format(c)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, cwd=cwd, shell=True).stdout.decode()
+                parsed_date = datetime.strptime(commit_date.strip(), '%Y-%m-%d %H:%M:%S %z')
+                parsed_date_str = parsed_date.strftime('%Y-%m-%d %H:%M:%S %z')
+
+
+                file_contents = ''
+                try:
+                    contents1 = subprocess.run(['git show {}:"{}"'.format(c, new_name)], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, cwd=cwd, shell=True)
+                    
+                    
+                    
+                    
+                    
+                    
+                    scratch_bytes_content = contents1.stdout
+                    
+                    
+                    if b'PK' not in scratch_bytes_content:
+                        continue
+
+                    decoded_vals = sp.decode_scratch_bytes(scratch_bytes_content)
+                    
+                    
+                    stats = sp.parse_scratch_modified(decoded_vals,new_name) if len(decoded_vals) > 0 else {"parsed_tree":[],"stats":{}}
+                    
+                     
+                except:
+                    stats = {"parsed_tree":[],"stats":{}}
+            
+                
+                nodes_count = 0 
+                edges_count = 0
+                try:
+                    nodes_count = stats["stats"]["number_of_nodes"]
+                    edges_count = stats["stats"]["number_of_edges"]
+                except:
+                    nodes_count = 0
+                    edges_count = 0
+               
+                json_output = json.dumps(stats, indent=4)
+
+                
+                hash_value = calculate_sha256(str(json_output))
+            
+                new_original_file_name = f.replace(",", "_COMMA_")
+                new_name = new_name.replace(",", "_COMMA_")
+            
+                insert_revision_statement = """INSERT INTO Revisions (Project_Name, File, Revision, Commit_SHA, Commit_Date, Hash, Nodes, Edges) VALUES(?,?,?,?,?,?,?,?);"""
+                insert_hash_statement = """INSERT INTO Contents (Hash,Content) VALUES(?,?);"""
+                tree_value = str(json_output)
+                conn,cur = get_connection2()
+                val = None
+                if conn != None:
+                    cur.execute(insert_revision_statement,(project_name,new_original_file_name,new_name,c,parsed_date_str,hash_value,nodes_count,edges_count))
+                    cur.execute(insert_hash_statement,(hash_value,tree_value))
+                    
+                else:
+                    if val != None:
+                        print("executed")
+                    print("connection failed")
+                conn.commit()
+    
         
 
 def main2(project_path: str):
@@ -385,9 +601,10 @@ def main2(project_path: str):
             proj_names.append(i)
         else:
             continue
-    projects_to_skip = get_all_projects_in_db()
-    train_projects,test_projects = train_test_split(proj_names,test_size=0.1,random_state=42)
-    print(type(train_projects))
+    projects_to_skip = get_all_projects_in_db_train()
+    projects_to_skip_test = get_all_projects_in_db()
+    train_projects,test_projects = train_test_split(proj_names,test_size=0.2)
+    
     for proj_name in train_projects:
         
         if proj_name not in projects_to_skip and proj_name != '' and len(proj_name) > 1:
@@ -404,8 +621,8 @@ def main2(project_path: str):
 
                 except Exception as e:
                     
-                    f = open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/exceptions4.txt", "a")
-                    #f = open("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos/exceptions4.txt","a")
+                    #f = open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/exceptions4.txt", "a")
+                    f = open("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos/exceptions4.txt","a")
                     f.write("{}\n".format(e))
                     f.close()
                     
@@ -416,6 +633,34 @@ def main2(project_path: str):
             print(f"skipped {proj_name}")
             continue
 
+    
+    for proj_name in test_projects:
+        
+        if proj_name not in projects_to_skip and proj_name != '' and len(proj_name) > 1:
+        #if  proj_name != '' and len(proj_name) > 1:
+            repo = f'{project_path}/{proj_name}'
+            main_branch = subprocess.run(['git rev-parse --abbrev-ref HEAD'], stdout=subprocess.PIPE, cwd=repo, shell=True)
+            main_branch = main_branch.stdout.decode("utf-8").strip('/n')[0:]
+            
+            
+            if len(main_branch) > 1 or main_branch != '' or main_branch != None and repo != '' or repo != None and len(repo) > 0 and len(main_branch) > 0:
+                try:
+                    
+                    get_revisions_and_run_parser_test(repo,main_branch,proj_name)
+
+                except Exception as e:
+                    
+                    #f = open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/exceptions4.txt", "a")
+                    f = open("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos/exceptions4.txt","a")
+                    f.write("{}\n".format(e))
+                    f.close()
+                    
+            else:
+                print("skipped or visited")
+                continue
+        else:
+            print(f"skipped {proj_name}")
+            continue
 
 
 #main2("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos")
