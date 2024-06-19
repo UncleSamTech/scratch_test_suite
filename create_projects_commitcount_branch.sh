73923eb7bf5a640f0f1d61a119e6ac4c35bfaa89
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-19 17:39:39 -0600

    wip

---
+++ create_projects_commitcount_branch.sh
@@ -0,0 +1,10 @@
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names.txt
+#INPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names.txt
+while read p; do
+    cd /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted/$p
+    #cd /Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos/$p
+    size=`git --no-pager log --all --pretty=tformat:"%H %P" | wc -l`
+    branch=`git rev-parse --abbrev-ref HEAD`
+    echo $p,$size,$branch >> /media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch.csv
+    #echo $p,$size,$branch >> /Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv
+done < $INPUT
---
+++ create_projects_table.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+import pandas as pd
+
+def create_projects(projects_path):
+    
+    for i in os.listdir(projects_path):
+        if len(i) > 1 and os.path.isdir(f'{projects_path}/{i}'):
+            #with open("all_projects_names.txt","a") as pn:
+            with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names.txt","a") as pn:
+                pn.write(f"{i}\n")
+        else:
+            continue
+
+
+def insert_into_projects_table(project_commit_branch_path):
+    #LOAD the data
+    df = pd.read_csv(project_commit_branch_path)
+
+    #clean the data
+    df.columns = df.columns.str.strip()
+
+    #connect to database
+    connection = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_main_train2.db")
+    #connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db")
+    #load data into file
+    df.to_sql("Projects", connection, if_exists='replace', index=False)
+
+#create_projects("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/files/repos")
+create_projects("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted")
+#/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects
+#insert_into_projects_table("/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv")
+insert_into_projects_table("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch.csv")
