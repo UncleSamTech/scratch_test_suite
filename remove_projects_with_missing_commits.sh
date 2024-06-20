5ba50a7911df9db71a56b13f23e745adefa2d0d9
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-20 15:06:06 -0600

    wip

--- remove_projects_with_missing_commits.sh
+++ remove_projects_with_missing_commits.sh
@@ -1,6 +1,6 @@
-INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch.csv
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_messages/commitsha_commitmessages_unique.csv
 #INPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv
-OUTPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch_cleaned.csv
+OUTPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_messages/commitsha_commitmessages_unique_cleaned.csv
 #OUTPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch_cleaned.csv
-awk -F, 'NF==3 && $1 != "" && $2 != "" && $3 != "" ' "$INPUT" > "$OUTPUT"
+awk -F, 'NF==2 && $1 != "" && $3 != "" && $4 != "" ' "$INPUT" > "$OUTPUT"
 #echo "Lines with missing columns have being removed and saved to $OUTPUT"
