2f911024ced71b4b9a1087200892f6f786538e77
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-19 18:25:15 -0600

    wip

--- remove_projects_with_missing_commits.sh
+++ remove_projects_with_missing_commits.sh
@@ -1,6 +1,6 @@
-#INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch.csv
-INPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv
-#OUTPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch_cleaned.csv
-OUTPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch_cleaned.csv
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch.csv
+#INPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv
+OUTPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names_commit_branch_cleaned.csv
+#OUTPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch_cleaned.csv
 awk -F, 'NF==3 && $1 != "" && $2 != "" && $3 != "" ' "$INPUT" > "$OUTPUT"
-echo "Lines with missing columns have being removed and saved to $OUTPUT"
+#echo "Lines with missing columns have being removed and saved to $OUTPUT"
