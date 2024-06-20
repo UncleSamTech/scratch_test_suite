9459f386349cc36d85b4f23ec73723c5857b14b3
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-20 15:10:01 -0600

    wip

--- remove_projects_with_missing_commits.sh
+++ remove_projects_with_missing_commits.sh
@@ -2,5 +2,5 @@ INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_mes
 #INPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch.csv
 OUTPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_messages/commitsha_commitmessages_unique_cleaned.csv
 #OUTPUT=/Users/samueliwuchukwu/Documents/thesis_project/scratch_test_suite/all_projects_names_commit_branch_cleaned.csv
-awk -F, 'NF==2 && $1 != "" && $3 != "" && $4 != "" ' "$INPUT" > "$OUTPUT"
+awk -F, 'NF==2' "$INPUT" > "$OUTPUT"
 #echo "Lines with missing columns have being removed and saved to $OUTPUT"
