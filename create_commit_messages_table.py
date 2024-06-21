c8ee6a823ed76f0e7b7cbc2253e049747efb374d
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-20 20:08:59 -0600

    wip

--- create_commit_messages_table.py
+++ create_commit_messages_table.py
@@ -2,14 +2,50 @@ import os
 import pandas as pd
 import sqlite3
 
-def insert_into_commit_messages(file_path):
-    df = pd.read_csv(file_path,lineterminator='\n')
-    print(df['Commit_Sha'])
-    df = df.columns.str.strip()
-    
-    connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db")
+def is_sha1(maybe_sha):
+    if len(maybe_sha) != 40:
+        return False
+    try:
+        sha_int = int(maybe_sha, 16)
+    except ValueError:
+        return False
+    return True
 
-    df.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
+def get_connection():
+    conn = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db",isolation_level=None)
+    cursor =  conn.cursor()
+    return conn,cursor
+
+def insert_into_commit_messages(file_path):
+    commit_sha = None
+    commit_message = None
+    with open(file_path,"r",encoding="utf",errors="ignore") as cm:
+        lines  = cm.readlines()
+        for line in lines:
+            complete_content = line.split(",")
+            
+            if len(complete_content) == 2:
+                commit_sha = complete_content[0] if is_sha1(complete_content[0]) else "None"
+                commit_message = complete_content[1] 
+            else:
+                commit_sha = complete_content[0] if is_sha1(complete_content[0]) else "None"
+                commit_message = "None"
+         
+            insert_commit_message = """INSERT INTO Commit_Message (Commit_Sha, Commit_Message) VALUES(?,?);"""
+            
+            conn,cur = get_connection()
+            val = None
+                
+            if conn != None:
+                cur.execute(insert_commit_message,(commit_sha,commit_message))               
+            else:
+                if val != None:
+                    print("executed")
+                print("connection failed")
+            conn.commit()
+
+        
+    
 
 
 insert_into_commit_messages("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_messages/commitsha_commitmessages_unique_cleaned.csv")
