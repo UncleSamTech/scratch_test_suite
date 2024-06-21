85b184404777ad9d5f224f2f953d431f7eeeb849
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-20 19:29:17 -0600

    wip

--- create_commit_messages_table.py
+++ create_commit_messages_table.py
@@ -9,7 +9,7 @@ def insert_into_commit_messages(file_path):
     
     connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db")
 
-    #df.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
+    df.to_sql("Commit_Messages", connection, if_exists='replace', index=False)
 
 
 insert_into_commit_messages("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/commit_messages/commitsha_commitmessages_unique_cleaned.csv")
