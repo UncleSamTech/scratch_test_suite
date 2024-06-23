b0638760b4a5882fcc40bf010d8ee7f80885a53b
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-23 17:03:30 -0600

    wip

---
+++ extract_parent_commit_commit_sha.py
@@ -0,0 +1,21 @@
+import os
+def parent_commit(path):
+    if os.path.isdir(path):
+        for i in os.listdir(path):
+            with open(os.path.join(path,i),'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    data = line.split(" ")
+                    commit_sha = data[0]
+                    parent_sha = data[1:]
+
+                    if len(parent_sha) == 0:
+                        with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commits_result.csv","a") as pc:
+                            pc.write(commit_sha + "," + "None\n")
+                    else:
+                        for p in parent_sha:
+                            with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/parent_commits_result.csv","a") as pc:
+                                pc.write(commit_sha + "," + f'{p}\n')
+                                
+parent_commit("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/parent_commits")
