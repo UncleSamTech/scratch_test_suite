f640d5ef321e512767f2c19ba715cbaac5b4a634
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-23 22:41:30 -0600

    wip

--- create_content_parent.py
+++ create_content_parent.py
@@ -7,7 +7,8 @@ from pathlib import Path
 import sqlite3
 import os
 
-conn = sqlite3.connect('/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_database.db')
+#conn = sqlite3.connect('/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_database.db')
+conn = sqlite3.connect('/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db')
 
 def is_sha1(maybe_sha):
     if len(maybe_sha) != 40:
@@ -18,6 +19,41 @@ def is_sha1(maybe_sha):
         return False
     return True
 
+def count_seperator(fn):
+    if isinstance(fn,str):
+        #return fn.strip().count("#")
+        return fn.strip().count("¬")
+
+def correct_code_replace(byte_val):
+    if (is_valid_encoding(byte_val)):
+        #decode the byte
+        
+        
+        decoded_byte = byte_val.decode('utf-8')
+        
+        
+        #replace values
+        replace_val = decoded_byte.replace('\xc2','¬') if '\xc2' in decoded_byte else decoded_byte
+        replace_val = replace_val.replace('\x00','¬') if '\x00' in replace_val else replace_val
+        
+        
+        #replace_val = decoded_byte.replace('\xc2','#').replace('\x00','#') if '\xc2' in decoded_byte or '\x00' in decoded_byte else decoded_byte
+        
+        #encode it back
+        encoded_byte = replace_val.encode('utf-8')
+        
+        #print(f"original {byte_val} after decode {decoded_byte} after replacement {replace_val} final value {encoded_byte}")
+      
+        return encoded_byte
+
+def is_valid_encoding(byte_string,encoding='utf-8'):
+    try:
+        decoded_string = byte_string.decode(encoding)
+        print(type(decoded_string))
+        return True
+    except UnicodeDecodeError:
+        print(type(decoded_string))
+        return False
 
 def get_parents_from_database(c):
     
@@ -46,19 +82,17 @@ def get_valid_parents_recursive(c, parents_of_c, commits_which_modified_file_f, 
 def get_revisions_and_run_parser(cwd, project_name, main_branch, debug=False):
     proc1 = subprocess.run(['git --no-pager log --pretty=tformat:"%H" {} --no-merges'.format(main_branch)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
     proc2 = subprocess.run(['xargs -I{} git ls-tree -r --name-only {}'], input=proc1.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
-    proc3 = subprocess.run(['grep -i "\.sb3$"'], input=proc2.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+    proc3 = subprocess.run(['grep -i "\\.sb3$"'], input=proc2.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
     proc4 = subprocess.run(['sort -u'], input=proc3.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
     filenames = proc4.stdout.decode().strip().split('\n')
 
-    if filenames is None or filenames  == [''] or len(filenames) == 0 or filenames == []:
-        #logging.error(f'no sb3 file found in {project_name} due to {logging.ERROR}')
-        return -1
-    else:
+    if len(filenames) > 1:
         # for all pd files in ths project
         for f in filenames:
             proc1 = subprocess.run(['git --no-pager log -z --numstat --follow --pretty=tformat:"{}¬%H" -- "{}"'.format(f,f)], stdout=subprocess.PIPE, cwd=cwd, shell=True)
             proc2 = subprocess.run(["cut -f3"], input=proc1.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
-            proc3 = subprocess.run(["sed 's/\d0/¬/g'"], input=proc2.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            #proc3 = subprocess.run(["sed 's/\d0/¬/g'"], input=proc2.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
+            proc3 = correct_code_replace(proc2.stdout)
             proc4 = subprocess.run(['xargs -0 echo'], input=proc3.stdout, stdout=subprocess.PIPE, cwd=cwd, shell=True)
             filename_shas = proc4.stdout.decode().strip().split('\n')
             filename_shas = [x for x in filename_shas if x != '']
@@ -78,7 +112,7 @@ def get_revisions_and_run_parser(cwd, project_name, main_branch, debug=False):
 
             # get filenames for each commit
             for fn in filename_shas: # start reversed, oldest to newest
-                separator_count = fn.strip().count('¬')
+                separator_count = count_seperator(fn)
                 split_line = fn.strip('¬').split('¬')
                 #print(split_line)
                 file_contents = ''
@@ -155,13 +189,13 @@ def get_revisions_and_run_parser(cwd, project_name, main_branch, debug=False):
             
 
                 if len(parents_of_c) == 0:
-                    with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/content_parents_1.txt", "a") as outfile:
+                    with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/content_parents/content_parents_1.txt", "a") as outfile:
                         outfile.write("{}_COMMA_{}_COMMA_{}_COMMA_{}\n".format(project_name, f, c, c))
                 else:
                     # we have a set of valid parents for c Get the node and edge count at each of these parents
                     for parent in parents_of_c:
 
-                        with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/content_parents_1.txt", "a") as outfile:
+                        with open("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/content_parents/content_parents_1.txt", "a") as outfile:
                             outfile.write("{}_COMMA_{}_COMMA_{}_COMMA_{}\n".format(project_name, f, c, parent))
 
         return 1
@@ -198,7 +232,7 @@ def main2(project_path: str):
                     
                 except Exception as e:
                     
-                    f = open("/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/content_parents/exceptions3.txt", "a")
+                    f = open("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/content_parents/exceptions3.txt", "a")
                     f.write("{}\n".format(e))
                     f.close()
                     #logging.error(f'skipped {project_name}  to {logging.ERROR}')
