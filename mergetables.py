cb2e7595d1890d33973e1ea417c8c9fee418c103
samueliwuchukwu <samueloel3486@gmail.com>
2024-06-27 12:53:14 -0600

    wip

--- mergetables.py
+++ mergetables.py
@@ -11,14 +11,14 @@ connection_new = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_fi
 cursor_new =  connection_new.cursor()
 
 #create a new table in the database
-cursor_new.execute("""CREATE TABLE IF NOT EXISTS Commit_Messages (
+cursor_new.execute("""CREATE TABLE IF NOT EXISTS Commit_Message (
   "Commit_SHA" TEXT,
    "Commit_Message" TEXT);""")
 
 #copy data from first table
-cursor1.execute("Select * from Commit_Messages;")
+cursor1.execute("Select * from Commit_Message;")
 rows1 = cursor1.fetchall()
-cursor_new.executemany("INSERT into Commit_Messages (Commit_SHA,Commit_Message) VALUES (?,?)",rows1)
+cursor_new.executemany("INSERT into Commit_Message (Commit_SHA,Commit_Message) VALUES (?,?)",rows1)
 
 #copy data from second database
 #cursor2.execute("Select * from Contents;")
