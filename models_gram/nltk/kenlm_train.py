18f8cb4ca9e79fa980656b7d20b4a74272533f8d
samueliwuchukwu <samueloel3486@gmail.com>
2024-08-29 14:13:31 -0600

    wip

--- models_gram/nltk/kenlm_train.py
+++ models_gram/nltk/kenlm_train.py
@@ -103,7 +103,7 @@ class kenlm_train:
         print(arpa_names)
         for index,arpa_file in enumerate(arpa_names):  
             arpa_file = arpa_file.strip()  
-            with open(arpa_file,"r",encoding="utf-8") as fr:
+            with open(f"{arpa_path}/{arpa_file}","r",encoding="utf-8") as fr:
                 lines = fr.readlines()
                 one_grams_seen = False
                 i = 0
@@ -239,7 +239,7 @@ kn = kenlm_train()
 
 
 #kn.create_vocab("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/arpas3/kenlmn_upd_order10.arpa","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/vocabs_folder/kenlm_sb3_order2.vocab")
-kn.create_vocab("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/main_vocabs_folder/kenlm_sb3_order","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/main_arpa/")
+kn.create_vocab("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/main_vocabs_folder/kenlm_sb3_order","/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/main_arpa")
 #/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/main_arpa
 #print(kn.test_kenlm("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/arpas_upd/kenlm_order2_model.arpa"))
 #model_evaluated = kn.test_kenlm("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/models_gram/kelmn/arpas_upd/kenlm_order2_model.arpa")
