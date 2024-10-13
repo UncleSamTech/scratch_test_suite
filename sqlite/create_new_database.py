30075f33047a777ead3d98b7c0caba4dd491432c
samueliwuchukwu <samueloel3486@gmail.com>
2024-10-12 19:24:05 -0600

    changes

--- sqlite/create_new_database.py
+++ sqlite/create_new_database.py
@@ -18,7 +18,7 @@ import sqlite3
 
 # step 3: create/connect to database
 #connection = sqlite3.connect("scratch_revisions_database.db")
-connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all_final.db")
+connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_all.db")
 #connection = sqlite3.connect("/Users/samueliwuchukwu/documents/scratch_database/scratch_revisions_main_train2.db")
 #connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_train3.db")
 #connection_test = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_main_test3.db")
@@ -30,30 +30,20 @@ connection = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/
 #df5.to_sql("Content_Parents",connection,if_exists='replace',index=False)
 #df_proj_load("Authors_Project",connection,if_exists='replace',index=False)
 
+content_parent_object =  connection.cursor()
+cont_parent_table = """CREATE TABLE Content_Parents(Project_Name,File,Commit_SHA,Content_Parent_SHA);"""
+content_parent_object.execute(cont_parent_table)
 
-curs =  connection.cursor()
-
-
-revision = """CREATE TABLE "Revisions" (
-  "Project_Name" TEXT,
-  "File" TEXT,
-  "Revision" TEXT,
-  "Commit_SHA" TEXT,
-  "Commit_Date" TEXT,
-  "Hash" TEXT,
-  "Nodes" INTEGER,
-  "Edges" INTEGER);"""
-curs.execute(revision)
-
-contents = """CREATE TABLE "Contents" (
-  "Hash" TEXT,
-  "Content" TEXT
-);"""
-curs.execute(contents)
-
-
-curs.execute('''CREATE UNIQUE INDEX "ix_Hashes_index" ON "Contents" ("Hash");''')
-
+'''
+revision_hash_index="""CREATE INDEX "sc_Revisions_Hashes_index" ON "Revisions" ("Hash"); """
+revision_project_index="""CREATE INDEX "sc_Revisions_Projects_index" ON "Revisions" ("Project_Name"); """
+revision_commit_index="""CREATE INDEX "sc_Revisions_Commit_index" ON "Revisions" ("Commit_SHA"); """
+project_project_name_index="""CREATE INDEX "sc_Projects_index" ON "Projects" ("Project_Name"); """
+authors_author_index="""CREATE INDEX "sc_Authors_index" ON "Authors" ("Commit_SHA"); """
+commit_messages_commit_sha_index="""CREATE INDEX "sc_Commit_Messages_index" ON "Commit_Messages" ("Commit_SHA"); """
+commit_parents_commitsha_index="""CREATE INDEX "ix_Commit_Parents_index" ON "Commit_Parents" ("Commit_SHA"); """
+content_parents_commit_sha_index="""CREATE INDEX "ix_Content_Parents_index" ON "Content_Parents" ("Commit_SHA"); """
+'''
 
 # step 5: close 
 connection.commit()
