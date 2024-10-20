93715470002cea98b6728379943ed2054948b555
samueliwuchukwu <samueloel3486@gmail.com>
2024-10-19 23:19:15 -0600

    wip

--- extract_parent_commit.sh
+++ extract_parent_commit.sh
@@ -1,6 +1,6 @@
 #INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/sb3_extracted_revisions/author_commit/auto_commit_data/projnames2.txt
-INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/projects/all_projects_names.txt
+INPUT=/media/crouton/siwuchuk/newdir/vscode_repos_files/all_extracted_projects.csv
 while read p; do
     cd /media/crouton/siwuchuk/newdir/vscode_repos_files/sb3projects_mirrored_extracted/$p
-    git --no-pager log --all --pretty=tformat:"%H %P" > /media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/parent_commits/$p.txt
+    git --no-pager log --all --pretty=tformat:"%H %P" > /media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/parent_commits_upd/$p.txt
 done < $INPUT
