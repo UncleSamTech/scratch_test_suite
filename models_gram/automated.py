c0908187fb059b1d74937e67e16d20d9db0dd28d
samueliwuchukwu <samueloel3486@gmail.com>
2025-01-28 15:24:01 -0700

    wi[

---
+++ models_gram/automated.py
@@ -0,0 +1,25 @@
+import os
+from sklearn.model_selection import train_test_split
+import sqlite3
+
+def get_all_hashes_from_projects(db_path):
+    pass
+
+
+def get_connection():
+    conn = sqlite3.connect("/media/crouton/siwuchuk/newdir/vscode_repos_files/scratch_test_suite/sqlite/scratch_revisions_cons_all.db",isolation_level=None)
+    cursor =  conn.cursor()
+    return conn,cursor
+
+def get_all_project_names():
+    #make connection
+    conn,curs = get_connection()
+    RETR_PROJ_QUERY = """SELECT project_name from projects;"""
+    curs.execute(RETR_PROJ_QUERY)
+    all_projects = curs.fetchall()
+    print(all_projects)
+    conn.commit()
+    conn.close()
+
+
+get_all_project_names()
