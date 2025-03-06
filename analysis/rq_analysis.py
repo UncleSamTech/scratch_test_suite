b55eb5ff8e84ad8f74dd9e376c11e9dea751b275
samueliwuchukwu <samueloel3486@gmail.com>
2025-03-06 10:08:45 -0700

    wip

--- analysis/rq_analysis.py
+++ analysis/rq_analysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #df = pd.read_csv("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/differences_nodes_edges/differences_nodes_edges_sb3_files_unique.csv")
-df = pd.read_csv("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/differences_nodes_edges/differences_nodes_edges_sb3_files_upd_3.csv")
+df = pd.read_csv("/media/crouton/siwuchuk/newdir/vscode_repos_files/thesis_record/differences_nodes_edges/differences_nodes_edges_sb3_files_upd_3_uniq.csv")
 df.replace([float('inf'), -float('inf')], pd.NA, inplace=True)
 df = df.dropna()
 
@@ -26,26 +26,26 @@ print("Edges <= 17: ", len(edges[edges <= 17]))
 
 
 nodes_jittered = nodes + jitter_nodes
-# plt.hist(nodes, color='lightblue', ec='black', bins=20)
-# plt.yscale('log')
-# plt.ticklabel_format(axis='x', style='plain')
-# plt.xlabel('Difference in Nodes Per Revision of a Scratch3 File')
-# plt.ylabel('Number of Total Revisions (Log Scale)')
-# plt.title('Histogram of Difference in Nodes Per Revision of a Scratch3 File')
-# #plt.show()
-# plt.savefig("diff_nodes_per_revision_distribution_upd.pdf")
-
-
-edges_jittered = edges + jitter_edges
-plt.hist(edges_jittered, color='lightblue', ec='black', bins=20)
+plt.hist(nodes, color='lightblue', ec='black', bins=20)
 plt.yscale('log')
 plt.ticklabel_format(axis='x', style='plain')
-plt.xlabel('Difference in Edges Per Revision of a Scratch3 File')
+plt.xlabel('Difference in Nodes Per Revision of a Scratch3 File')
 plt.ylabel('Number of Total Revisions (Log Scale)')
-plt.title('Histogram of Difference in Edges Per Revision of a Scratch3 File')
+plt.title('Histogram of Difference in Nodes Per Revision of a Scratch3 File')
 #plt.show()
-plt.savefig("diff_edges_per_revision_distribution_upd.pdf")
+plt.savefig("diff_nodes_per_revision_distribution_upd_uniq.pdf")
 
-# median value
-print(df["Diff_Nodes"].sort_values().median())
-print(df["Diff_Edges"].sort_values().median())
+
+# edges_jittered = edges + jitter_edges
+# plt.hist(edges_jittered, color='lightblue', ec='black', bins=20)
+# plt.yscale('log')
+# plt.ticklabel_format(axis='x', style='plain')
+# plt.xlabel('Difference in Edges Per Revision of a Scratch3 File')
+# plt.ylabel('Number of Total Revisions (Log Scale)')
+# plt.title('Histogram of Difference in Edges Per Revision of a Scratch3 File')
+# #plt.show()
+# plt.savefig("diff_edges_per_revision_distribution_upd.pdf")
+
+# # median value
+# print(df["Diff_Nodes"].sort_values().median())
+# print(df["Diff_Edges"].sort_values().median())
