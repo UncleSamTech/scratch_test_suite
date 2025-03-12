b2b63e01dc10cd03828dd4bd9da9382c674cc444
samueliwuchukwu <samueloel3486@gmail.com>
2025-03-12 13:48:34 -0600

    wip

--- models_gram/nltk/scratch_train_mle_parallel_update_sma.py
+++ models_gram/nltk/scratch_train_mle_parallel_update_sma.py
@@ -1062,7 +1062,8 @@ class scratch_train_mle:
         # return scratch_predicted_next_token, top_10_tokens_scores
 
 tr_scr = scratch_train_mle()
-tr_scr.scratch_evaluate_model_small("/media/crouton/siwuchuk/newdir/vscode_repos_files/method/output_test/sample/scratch_test_set_20_2_1_proc_100.txt","nltk20_2_1.pkl","/media/crouton/siwuchuk/newdir/vscode_repos_files/method/models/nltk/logs/20/res","/media/crouton/siwuchuk/newdir/vscode_repos_files/method/models/nltk/models/20/samp",1,2,20)
+
+tr_scr.scratch_evaluate_model_small("/media/crouton/siwuchuk/newdir/vscode_repos_files/method/output_test/sample/scratch_test_set_20_2_1_proc_100.txt","nltk_20_2_1.pkl","/media/crouton/siwuchuk/newdir/vscode_repos_files/method/models/nltk/logs/20/res","/media/crouton/siwuchuk/newdir/vscode_repos_files/method/models/nltk/models/20",1,2,20)
 
 #tr_scr.train_mle_new_upd("/media/crouton/siwuchuk/newdir/vscode_repos_files/method/output_train/samp/scratch_train_set_20_2_1_proc.txt",2,"nltk","/media/crouton/siwuchuk/newdir/vscode_repos_files/method/models/nltk/models/20/samp",20,1)
 # def main():
