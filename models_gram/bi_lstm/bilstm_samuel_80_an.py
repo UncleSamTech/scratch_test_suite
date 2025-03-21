17439fae04dfd29b4f63041c6c3285a6e26cccf5
samueliwuchukwu <samueloel3486@gmail.com>
2025-03-21 17:57:35 -0600

    wip

--- models_gram/bi_lstm/bilstm_samuel_80_an.py
+++ models_gram/bi_lstm/bilstm_samuel_80_an.py
@@ -241,7 +241,7 @@ class bilstm_cybera:
 
 # # Example usage
 cl_ob = bilstm_cybera()
-cl_ob.view_model_summary("/mnt/siwuchuk/vscode/models/bilstm/model/30/main_bilstm_scratch_model_150embedtime1_main_sample_project30_6_1.keras")
+cl_ob.view_model_summary("/mnt/siwuchuk/vscode/models/bilstm/model/30/main_bilstm_scratch_model_150embedtime1_main_sample_project30_6_2.keras")
 # sample = (
 #     "/mnt/siwuchuk/thesis/another/kenlm/output_train",
 #     "/mnt/siwuchuk/thesis/another/bilstm/models/80/",
